bfa19d59949e058c77d2f8c2b733232b86b179e4
Lik Eduard <feft99@gmail.com>
2021-04-10 02:55:20 +0600

    set setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='pwdgenerator',
+  version='0.0.1',
+  description='This is the package about password generation.',
+  url='https://github.com/feftio/pwdgenerator',
+  author='Lik Eduard',
+  author_email='feft99@gmail.com',
+  keywords='password',
+  packages=['pwdgenerator']
+  install_requires=[
+    'pytest'
+  ]
+)
