8e0be2cffcc7504f60ab9e1a25d5dce547588e05
Lik Eduard <feft99@gmail.com>
2021-04-10 16:45:53 +0600

    tests for weeks and summary

--- tests/test_duration.py
+++ tests/test_duration.py
@@ -1,10 +1,12 @@
 from pwdgenerator.duration import Duration
 
+
 def test_minutes():
     assert Duration(minutes=30).inMinutes == 30
     assert Duration(minutes=30).inHours == 0.5
     assert Duration(minutes=1440).inDays == 1
     assert Duration(minutes=10080).inWeeks == 1
+
 
 def test_hours():
     assert Duration(hours=1).inMinutes == 60
@@ -12,9 +14,20 @@ def test_hours():
     assert Duration(hours=24).inDays == 1
     assert Duration(hours=168).inWeeks == 1
 
+
 def test_days():
     assert Duration(days=1).inMinutes == 1440
     assert Duration(days=1).inHours == 24
     assert Duration(days=31).inDays == 31
     assert Duration(days=14).inWeeks == 2
-    
+
+
+def test_weeks():
+    assert Duration(weeks=1).inMinutes == 10080
+    assert Duration(weeks=1).inHours == 168
+    assert Duration(weeks=1).inDays == 7
+    assert Duration(weeks=2).inWeeks == 2
+
+
+def test_summary():
+    assert Duration(minutes=1440, hours=24, days=1, weeks=1).inDays == 10
