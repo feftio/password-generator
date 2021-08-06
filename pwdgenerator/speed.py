bcd96c37936a97e31b2e5bbe76c2a960738d0d1e
Eduard Lik <feft99@gmail.com>
2021-08-06 20:10:52 +0600

    Add type hints, test __mul__ method

--- pwdgenerator/speed.py
+++ pwdgenerator/speed.py
@@ -1,23 +1,29 @@
+from __future__ import annotations
+import typing as t
 from pwdgenerator.duration import Duration
 
 
 class Speed:
-
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, value: float):
+        self.value: float = value
 
     @property
-    def aMinute(self):
+    def aMinute(self) -> Duration:
         return self.value / Duration(minutes=1)
 
     @property
-    def aHour(self):
+    def aHour(self) -> Duration:
         return self.value / Duration(hours=1)
 
     @property
-    def aDay(self):
+    def aDay(self) -> Duration:
         return self.value / Duration(days=1)
 
     @property
-    def aWeek(self):
+    def aWeek(self) -> Duration:
         return self.value / Duration(weeks=1)
+
+
+if __name__ == '__main__':
+    print(Speed(60).aHour * Duration(hours=1))
+    print(Duration(hours=1) * (60 / Duration(hours=1)))
